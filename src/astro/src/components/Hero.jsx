1d7196320de9d4d9d1b6445054a54afe0f3cc135
jstavh <jstavh@gmail.com>
2022-08-25 11:47:01 -0400

    duplicate repo, change background, new image, changing codebase

--- src/astro/src/components/Hero.jsx
+++ src/astro/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <section className='hero'>
             <div className='container'>
                 <header>
-                    <h1>Granola</h1>
+                    <h1>Granola's Projects</h1>
                     <p>We make winning software teams.</p>
                     <p>Granola is a software consultancy. We make winning teams using our expertise in leadership, DevOps, Web3, distributed systems, functional programming, and data engineering.</p>
                     <p>Say hello: <a href="mailto:hello@granola.team">hello@granola.team</a></p>
