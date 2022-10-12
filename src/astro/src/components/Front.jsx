f2b2f2cbe6e57dd6973b71f6ab0d750eae2f5af1
jstavh <jstavh@gmail.com>
2022-10-12 10:18:51 -0400

    link to OCS code

--- src/astro/src/components/Front.jsx
+++ src/astro/src/components/Front.jsx
@@ -3,7 +3,7 @@ import '../styles/styles.css';
 const Front = () => {
     return (
         <section className="min-h-[90%] flex flex-col items-center">
-            <div class="pb-[15px] overflow-auto">
+            <div className="pb-[15px] overflow-auto">
             <header><h1 className="mt-12 text-center text-5xl text-white">
                     Granola's Projects</h1></header>
             <div className="flex flex-col mt-6 px-12 justify-center">
@@ -17,9 +17,11 @@ const Front = () => {
                     </a>
                     <a href="/" className="card text-3xl text-white hover:text-logoOrange
                     focus:text-logoOrange active:text-logoOrange">
-                        Granola Blog Posts and Articles &rarr;
+                        Mina On-Chain Signals &rarr;
                         <br></br><br></br>
-                        <span className="text-2xl">Our views and what's next.</span>
+                        <span className="text-2xl">
+                            Source code and election results.
+                        </span>
                     </a>
                 </div>
             </div>
