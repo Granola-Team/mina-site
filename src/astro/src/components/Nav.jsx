6e42fdc47e7a2a0143c188c331c50584acf493e9
Jason Hoffman <106464930+jstavh@users.noreply.github.com>
2022-10-14 18:31:00 -0400

    new homepage for mina.granola.team (#1)
    * new homepage for mina.granola.team
    * link to OCS code
    * change nav-bar and OCS pointing

--- src/astro/src/components/Nav.jsx
+++ src/astro/src/components/Nav.jsx
@@ -1,12 +1,20 @@
+import '../styles/styles.css';
+import '../../public/images/GranolaHorizontalDarkBg.png'
+
 const Nav = () => {
     return (
-        <ul>
-            <li>About</li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>Contact</li>
-        </ul>
+        <nav className="flex justify-between border-b-[0.03rem] border-solid 
+            border-white border-t-0 border-x-0 overflow-auto">
+                <div className="flex justify-start">
+                    <a href="/" ><img className="h-[3rem] w-[9rem] sm:h-20 sm:w-60" 
+                        src="../../images/GranolaHorizontalDarkBg.png" /></a>
+                </div>
+                <div className="flex justify-end items-center mr-6 gap-6">
+                    <a href="mailto:hello@granola.team" 
+                        className="text-xl sm:text-2xl text-white no-underline">Contact</a>
+                </div>
+        </nav>
     )
 }
 
-export default Nav; 
+export default Nav;
